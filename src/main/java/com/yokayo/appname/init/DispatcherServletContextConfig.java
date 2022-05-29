3d7924a30cda8aa37782677fccc3c5d09b151d91
Yokayo <52032299+Yokayo@users.noreply.github.com>
2022-05-30 00:56:04 +0700

    Update DispatcherServletContextConfig.java

--- src/main/java/com/yokayo/appname/init/DispatcherServletContextConfig.java
+++ src/main/java/com/yokayo/appname/init/DispatcherServletContextConfig.java
@@ -1,7 +1,6 @@
 package com.yokayo.appname.init;
 
 import javax.annotation.PreDestroy;
-import javax.inject.Inject;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.context.annotation.Bean;
